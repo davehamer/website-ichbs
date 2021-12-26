f84446b392ffa15cbf1479baa335bd6f11568a7a
root <root@IDYLAP.localdomain>
2021-12-26 18:50:00 +0100

    add bootstrap and boilerplate index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,26 @@
-<?php
-
-echo("Hello World!");
-echo("EOF");
-?>#
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>icbhs</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+	  <script src="js/bootstrap.bundle.min.js"></script>
+    <div class="container">
+      <div class="row align-items-center text-center">
+        <div class="col">
+          &nbsp;
+        </div>
+        <div class="col-4">
+          Main content
+        </div>
+        <div class="col">
+          &nbsp;
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
